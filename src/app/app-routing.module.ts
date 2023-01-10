0c9d849421dbca2ce78da2278cdb6e01809912ca
fsd5802 <fsd5802@gmail.com>
2023-01-10 09:08:26 +0200

    routing fix

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -6,7 +6,7 @@ import { RouterModule, Routes } from '@angular/router';
 const routes: Routes = [
     {
       path: '',
-      redirectTo: 'fruits/home',
+      redirectTo: 'posts/home',
       pathMatch: 'full',
     }    
   ];
