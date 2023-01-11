eaa9a973c228c9a45cba848c55a30801180a92b3
fsd5802 <fsd5802@gmail.com>
2023-01-11 15:54:58 +0200

    observable

--- src/app/fruits/fruits.service.ts
+++ src/app/fruits/fruits.service.ts
@@ -1,14 +1,14 @@
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 import { Fruits } from './fruits';
- 
+import { Observable } from 'rxjs';
 @Injectable({
   providedIn: 'root',
 })
 export class FruitsService {
   constructor(private http: HttpClient) {}
 
-  get() {
+  get():Observable<any>  {
     return this.http.get<any>('http://localhost:3000/fruits');
   }
 
